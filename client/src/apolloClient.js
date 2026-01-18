618808df56a9cbe005fd0f281b1b3d63dfcbd331
Ketan Muttelwar <ketanmuttelwar62@gmail.com>
2026-01-18 07:08:30 +0000

    New URI Render

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink  } from '@apollo/client';
 
-const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL || 'https://super-giggle-q7vwx56ppq6fx56j-4000.app.github.dev/';
+const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL || 'https://emp-mgm-graphql-apolloserver-mongodb.onrender.com/';
 
 const client = new ApolloClient({
   link: new HttpLink ({ uri: GRAPHQL_URL }),
